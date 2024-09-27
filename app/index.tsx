2e6ffcf9f2b75144da387f7640d65826327ed010
Boi <anhtuan03.dev@gmail.com>
2024-09-27 21:22:51 +0700

    init project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const HomePage = () => {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-rose-500 text-xl font-pbold'>Aora</Text>
+    </View>
+  )
+}
+
+export default HomePage
