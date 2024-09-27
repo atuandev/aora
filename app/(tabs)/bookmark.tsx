76f0134da304ef0ab1fa2df0d04a98b31136722a
Boi <anhtuan03.dev@gmail.com>
2024-09-27 22:09:24 +0700

    feat: add file structure

---
+++ app/(tabs)/bookmark.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const BookMark = () => {
+  return (
+    <View>
+      <Text>BookMark</Text>
+    </View>
+  )
+}
+
+export default BookMark
--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 import { Text, View } from 'react-native'
 
 const HomePage = () => {
   return (
     <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-rose-500 text-xl font-pbold'>Aora</Text>
+      <Text className='text-xl font-pbold'>Aora</Text>
+      <StatusBar style='auto' />
+      <Link href='/home' className='text-blue-500'>Home</Link>
     </View>
   )
 }
